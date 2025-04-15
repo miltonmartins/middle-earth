c1a57c7615d14afff49eae697e4f72fe482b101f
Milton Martins <milton.martins.dev@gmail.com>
2025-04-15 08:38:05 -0300

    feat: added splash screen

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.okhttp)
+            implementation(libs.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
